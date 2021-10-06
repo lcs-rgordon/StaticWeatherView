626c195f388eb0a76492e1cec4268e6bc4266400
Russell Gordon <github@russellgordon.ca>
2021-10-05 20:13:56 -0400

    Initial Commit

---
+++ StaticWeatherView/StaticWeatherViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  StaticWeatherViewApp.swift
+//  StaticWeatherView
+//
+//  Created by Russell Gordon on 2021-10-05.
+//
+
+import SwiftUI
+
+@main
+struct StaticWeatherViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
